06027b1734fbe681cd80206aa0597a243cc0d884
sudipta896 <sudiptamondal86106@gmail.com>
2026-05-07 16:27:22 +0530

    Expection error and logg handeling completed

---
+++ NETWORK_SEQUIRITY/logging/logger.py
@@ -0,0 +1,9 @@
+import logging
+import os
+from datetime import datetime
+LOG_FILE =   f'{datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}.log'
+LOG_PATH = os.path.join(os.getcwd(),"logs")
+os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
+LOG_FILE_PATH = os.path.join(LOG_PATH,LOG_FILE)
+logging.basicConfig(filename=LOG_FILE_PATH,  format='%(asctime)s - %(levelname)s - %(message)s',level=logging.INFO)
+
